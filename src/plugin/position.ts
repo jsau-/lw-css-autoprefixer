bc9eadd461e66e614c5ee319f6a566c190d376e7
Jon Saunders <jon@jsau.co.uk>
2022-09-11 01:46:47 +0100

    Simplify position plugin

--- src/plugin/position.ts
+++ src/plugin/position.ts
@@ -1,11 +1,9 @@
 import { Plugin } from '../prefixer';
-import { addVendorPrefixes } from '../util/addVendorPrefixes';
-import { Vendor } from '../Vendor';
 
 export const position: Plugin = (property, value) => {
   if (property !== 'position' || value !== 'sticky') {
     return;
   }
 
-  return addVendorPrefixes(value, Vendor.wk).map((prefixedVal) => [property, prefixedVal]);
+  return [['position', '-webkit-sticky']];
 };
